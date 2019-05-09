e6bd82284d3e4bb2318ca00bcdc6a1e71cecfe11
contactpeterchung.com <peterchung@Peters-MacBook-Air.local>
2019-05-09 11:33:48 -0600

    add git help menu

--- main.rb
+++ main.rb
@@ -1,22 +1,25 @@
+require "colorize"
+
 def puts_git(cmd)
     puts `git #{cmd} -h`
     menu
   end
   
   def menu
-    puts '1: Enter git command'
-    puts '2: Exit'
+    puts "MAIN MENU".colorize(:cyan)
+    puts '1: Enter git command'.colorize(:yellow)
+    puts '2: Exit'.colorize(:yellow)
     choice = gets.to_i
     case choice
     when 1
-      puts 'Enter git command'
+      puts 'Enter git command'.colorize(:green)
       puts_git(gets.strip)
       menu
     when 2
-        puts "Thanks for using our program!"
+        puts "Thanks for using our program!".colorize(:blue)
       exit
     else
-      puts 'Invalid choice'
+      puts 'Invalid choice'.colorize(:red)
       menu
     end
   end
